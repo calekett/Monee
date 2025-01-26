cf0427e61246c5f2959d2d0639907f8d6c7638f4
Cale Kettner <calekett@tamu.edu>
2025-01-26 00:36:20 -0600

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#313131] text-white">
-      <h1 className="text-4xl font-bold mb-4">Welcome to the Homepage</h1>
-      <p className="text-gray-300 mb-8">Click the button below to sign in.</p>
+      <h1 className="text-4xl font-bold mb-4">welcome to monee.</h1>
+      <p className="text-gray-300 mb-8">your new financial partner.</p>
       <Link href="/signin">
         <button className="px-6 py-2 bg-[#2cd3a7] text-white rounded-lg hover:bg-[#55f86b]">
           Go to Sign In
